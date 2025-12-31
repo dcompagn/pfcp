a7aa82f4b112c36334c18645272513dff44e4dfe
Davide Compagnin <dcompagn@gmail.com>
2025-12-31 09:20:46 -0500

    Add CHV4 and CHV6 fields to UEIPAddress type

--- pfcpType/UEIPAddress.go
+++ pfcpType/UEIPAddress.go
@@ -6,18 +6,25 @@ import (
 )
 
 type UEIPAddress struct {
-	Ipv6d                    bool
-	Sd                       bool
-	V4                       bool
-	V6                       bool
-	Ipv4Address              net.IP
-	Ipv6Address              net.IP
-	Ipv6PrefixDelegationBits uint8
+	Ip6pl                    bool   // Bit 7 - IPv6 Prefix Length present
+	Chv6                     bool   // Bit 6 - Choose IPv6
+	Chv4                     bool   // Bit 5 - Choose IPv4
+	Ipv6d                    bool   // Bit 4 - IPv6 Prefix Delegation
+	Sd                       bool   // Bit 3 - Source/Destination
+	V4                       bool   // Bit 2 - IPv4 address present
+	V6                       bool   // Bit 1 - IPv6 address present
+	Ipv4Address              net.IP // Octets m to (m+3)
+	Ipv6Address              net.IP // Octets p to (p+15)
+	Ipv6PrefixDelegationBits uint8  // Octet r
+	Ipv6PrefixLength         uint8  // Octet s
 }
 
 func (u *UEIPAddress) MarshalBinary() (data []byte, err error) {
 	// Octet 5
-	tmpUint8 := btou(u.Ipv6d)<<3 |
+	tmpUint8 := btou(u.Ip6pl)<<6 |
+		btou(u.Chv6)<<5 |
+		btou(u.Chv4)<<4 |
+		btou(u.Ipv6d)<<3 |
 		btou(u.Sd)<<2 |
 		btou(u.V4)<<1 |
 		btou(u.V6)
@@ -39,9 +46,14 @@ func (u *UEIPAddress) MarshalBinary() (data []byte, err error) {
 		data = append(data, u.Ipv6Address.To16()...)
 	}
 
-	// Octet r
-	if u.V6 && u.Ipv6d {
+	// Octet r - IPv6 Prefix Delegation Bits
+	if (u.V6 || u.Chv6) && u.Ipv6d {
 		data = append(data, u.Ipv6PrefixDelegationBits)
+	}
+
+	// Octet s - IPv6 Prefix Length
+	if (u.V6 || u.Chv6) && !u.Ipv6d && u.Ip6pl {
+		data = append(data, u.Ipv6PrefixLength)
 	}
 
 	return data, nil
@@ -55,6 +67,9 @@ func (u *UEIPAddress) UnmarshalBinary(data []byte) error {
 	if length < idx+1 {
 		return fmt.Errorf("inadequate TLV length: %d", length)
 	}
+	u.Ip6pl = utob(data[idx] & BitMask7)
+	u.Chv6 = utob(data[idx] & BitMask6)
+	u.Chv4 = utob(data[idx] & BitMask5)
 	u.Ipv6d = utob(data[idx] & BitMask4)
 	u.Sd = utob(data[idx] & BitMask3)
 	u.V4 = utob(data[idx] & BitMask2)
@@ -79,12 +94,21 @@ func (u *UEIPAddress) UnmarshalBinary(data []byte) error {
 		idx = idx + net.IPv6len
 	}
 
-	// Octet r
-	if u.V6 && u.Ipv6d {
+	// Octet r - IPv6 Prefix Delegation Bits
+	if (u.V6 || u.Chv6) && u.Ipv6d {
 		if length < idx+1 {
 			return fmt.Errorf("inadequate TLV length: %d", length)
 		}
 		u.Ipv6PrefixDelegationBits = data[idx]
+		idx = idx + 1
+	}
+
+	// Octet s - IPv6 Prefix Length
+	if (u.V6 || u.Chv6) && !u.Ipv6d && u.Ip6pl {
+		if length < idx+1 {
+			return fmt.Errorf("inadequate TLV length: %d", length)
+		}
+		u.Ipv6PrefixLength = data[idx]
 		idx = idx + 1
 	}
 
--- pfcpType/UEIPAddress_test.go
+++ pfcpType/UEIPAddress_test.go
@@ -9,6 +9,9 @@ import (
 
 func TestMarshalUEIPAddress(t *testing.T) {
 	testData := UEIPAddress{
+		Ip6pl:                    false,
+		Chv6:                     false,
+		Chv4:                     false,
 		Ipv6d:                    true,
 		Sd:                       true,
 		V4:                       true,
@@ -30,6 +33,9 @@ func TestUnmarshalUEIPAddress(t *testing.T) {
 
 	assert.Nil(t, err)
 	expectData := UEIPAddress{
+		Ip6pl:                    false,
+		Chv6:                     false,
+		Chv4:                     false,
 		Ipv6d:                    true,
 		Sd:                       true,
 		V4:                       true,
@@ -37,6 +43,117 @@ func TestUnmarshalUEIPAddress(t *testing.T) {
 		Ipv4Address:              net.ParseIP("12.34.56.78").To4(),
 		Ipv6Address:              net.ParseIP("2001:db8::68").To16(),
 		Ipv6PrefixDelegationBits: 4,
+	}
+	assert.Equal(t, expectData, testData)
+}
+
+func TestMarshalUEIPAddressWithCHV6(t *testing.T) {
+	testData := UEIPAddress{
+		Ip6pl:                    false,
+		Chv6:                     true,
+		Chv4:                     false,
+		Ipv6d:                    true,
+		Sd:                       false,
+		V4:                       false,
+		V6:                       false,
+		Ipv6PrefixDelegationBits: 8,
+	}
+	buf, err := testData.MarshalBinary()
+
+	assert.Nil(t, err)
+	// Octet 5: CHV6=1(bit6), Ipv6d=1(bit4) => 0b00101000 = 40
+	assert.Equal(t, []byte{40, 8}, buf)
+}
+
+func TestUnmarshalUEIPAddressWithCHV6(t *testing.T) {
+	buf := []byte{40, 8}
+	var testData UEIPAddress
+	err := testData.UnmarshalBinary(buf)
+
+	assert.Nil(t, err)
+	expectData := UEIPAddress{
+		Ip6pl:                    false,
+		Chv6:                     true,
+		Chv4:                     false,
+		Ipv6d:                    true,
+		Sd:                       false,
+		V4:                       false,
+		V6:                       false,
+		Ipv6PrefixDelegationBits: 8,
+	}
+	assert.Equal(t, expectData, testData)
+}
+
+func TestMarshalUEIPAddressWithIP6PL(t *testing.T) {
+	testData := UEIPAddress{
+		Ip6pl:            true,
+		Chv6:             false,
+		Chv4:             false,
+		Ipv6d:            false,
+		Sd:               false,
+		V4:               false,
+		V6:               true,
+		Ipv6Address:      net.ParseIP("2001:db8::1").To16(),
+		Ipv6PrefixLength: 56,
+	}
+	buf, err := testData.MarshalBinary()
+
+	assert.Nil(t, err)
+	// Octet 5: IP6PL=1(bit7), V6=1(bit1) => 0b01000001 = 65
+	assert.Equal(t, []byte{65, 32, 1, 13, 184, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 56}, buf)
+}
+
+func TestUnmarshalUEIPAddressWithIP6PL(t *testing.T) {
+	buf := []byte{65, 32, 1, 13, 184, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 56}
+	var testData UEIPAddress
+	err := testData.UnmarshalBinary(buf)
+
+	assert.Nil(t, err)
+	expectData := UEIPAddress{
+		Ip6pl:            true,
+		Chv6:             false,
+		Chv4:             false,
+		Ipv6d:            false,
+		Sd:               false,
+		V4:               false,
+		V6:               true,
+		Ipv6Address:      net.ParseIP("2001:db8::1").To16(),
+		Ipv6PrefixLength: 56,
+	}
+	assert.Equal(t, expectData, testData)
+}
+
+func TestMarshalUEIPAddressWithCHV4(t *testing.T) {
+	testData := UEIPAddress{
+		Ip6pl: false,
+		Chv6:  false,
+		Chv4:  true,
+		Ipv6d: false,
+		Sd:    false,
+		V4:    false,
+		V6:    false,
+	}
+	buf, err := testData.MarshalBinary()
+
+	assert.Nil(t, err)
+	// Octet 5: CHV4=1(bit5) => 0b00010000 = 16
+	assert.Equal(t, []byte{16}, buf)
+}
+
+func TestUnmarshalUEIPAddressWithCHV4(t *testing.T) {
+	buf := []byte{16}
+	var testData UEIPAddress
+	err := testData.UnmarshalBinary(buf)
+
+	assert.Nil(t, err)
+	expectData := UEIPAddress{
+		Ip6pl: false,
+		Chv6:  false,
+		Chv4:  true,
+		Ipv6d: false,
+		Sd:    false,
+		V4:    false,
+		V6:    false,
 	}
 	assert.Equal(t, expectData, testData)
 }
