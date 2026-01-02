3201f399eb2a1581ef0970d5945d443559236dba
Davide Compagnin <dcompagn@gmail.com>
2026-01-02 10:22:31 -0500

    Fix OHR marshalling

--- pfcpType/OuterHeaderRemoval.go
+++ pfcpType/OuterHeaderRemoval.go
@@ -4,37 +4,58 @@ import (
 	"fmt"
 )
 
+// Outer Header Removal Description values (Table 8.2.64-1)
 const (
-	OuterHeaderRemovalGtpUUdpIpv4 uint8 = iota
-	OuterHeaderRemovalGtpUUdpIpv6
-	OuterHeaderRemovalUdpIpv4
-	OuterHeaderRemovalUdpIpv6
+	OuterHeaderRemovalGtpUUdpIpv4  uint8 = 0 // GTP-U/UDP/IPv4
+	OuterHeaderRemovalGtpUUdpIpv6  uint8 = 1 // GTP-U/UDP/IPv6
+	OuterHeaderRemovalUdpIpv4      uint8 = 2 // UDP/IPv4
+	OuterHeaderRemovalUdpIpv6      uint8 = 3 // UDP/IPv6
+	OuterHeaderRemovalIpv4         uint8 = 4 // IPv4
+	OuterHeaderRemovalIpv6         uint8 = 5 // IPv6
+	OuterHeaderRemovalGtpUUdpIp    uint8 = 6 // GTP-U/UDP/IP (regardless IPv4 or IPv6)
+	OuterHeaderRemovalVlanStagPop  uint8 = 7 // VLAN TAG POP (removal of one VLAN tag, S-TAG or C-TAG)
+	OuterHeaderRemovalVlanStagCtag uint8 = 8 // VLAN TAGs POP-POP (removal of 2 VLAN tags, S-TAG and C-TAG)
+)
+
+// GTP-U Extension Header Deletion flags (Table 8.2.64-2)
+// Bitmask indicating which GTP-U extension headers should be deleted
+const (
+	GtpuExtHeaderDeletionPduSessionContainer uint8 = 1 << 0 // Bit 1: PDU Session Container
 )
 
 type OuterHeaderRemoval struct {
 	OuterHeaderRemovalDescription uint8
+	// GTP-U Extension Header Deletion field (octet 6) - optional
+	// Present only when GTP-U extension headers need to be deleted from incoming GTP-PDUs
+	GtpuExtensionHeaderDeletion *uint8
 }
 
 func (o *OuterHeaderRemoval) MarshalBinary() (data []byte, err error) {
-	// Octet 5
+	// Octet 5: Outer Header Removal Description
 	data = append([]byte(""), o.OuterHeaderRemovalDescription)
 
+	// Octet 6: GTP-U Extension Header Deletion (optional)
+	if o.GtpuExtensionHeaderDeletion != nil {
+		data = append(data, *o.GtpuExtensionHeaderDeletion)
+	}
 	return data, nil
 }
 
 func (o *OuterHeaderRemoval) UnmarshalBinary(data []byte) error {
 	length := uint16(len(data))
 
-	var idx uint16 = 0
-	// Octet 5
-	if length < idx+1 {
+	if length < 1 {
 		return fmt.Errorf("inadequate TLV length: %d", length)
 	}
-	o.OuterHeaderRemovalDescription = data[idx]
-	idx = idx + 1
 
-	if length != idx {
-		return fmt.Errorf("inadequate TLV length: %d", length)
+	// Octet 5: Outer Header Removal Description
+	o.OuterHeaderRemovalDescription = data[0]
+
+	// Octet 6: GTP-U Extension Header Deletion (optional)
+	// Present only if explicitly specified to delete GTP-U extension header(s)
+	if length >= 2 {
+		o.GtpuExtensionHeaderDeletion = new(uint8)
+		*o.GtpuExtensionHeaderDeletion = data[1]
 	}
 
 	return nil
--- pfcpType/OuterHeaderRemoval_test.go
+++ pfcpType/OuterHeaderRemoval_test.go
@@ -16,6 +16,18 @@ func TestMarshalOuterHeaderRemoval(t *testing.T) {
 	assert.Equal(t, []byte{OuterHeaderRemovalGtpUUdpIpv4}, buf)
 }
 
+func TestMarshalOuterHeaderRemovalWithExtHeaderDeletion(t *testing.T) {
+	extHeaderDeletion := GtpuExtHeaderDeletionPduSessionContainer
+	testData := OuterHeaderRemoval{
+		OuterHeaderRemovalDescription: OuterHeaderRemovalGtpUUdpIpv4,
+		GtpuExtensionHeaderDeletion:   &extHeaderDeletion,
+	}
+	buf, err := testData.MarshalBinary()
+
+	assert.Nil(t, err)
+	assert.Equal(t, []byte{OuterHeaderRemovalGtpUUdpIpv4, GtpuExtHeaderDeletionPduSessionContainer}, buf)
+}
+
 func TestUnmarshalOuterHeaderRemoval(t *testing.T) {
 	buf := []byte{OuterHeaderRemovalGtpUUdpIpv4}
 	var testData OuterHeaderRemoval
@@ -26,4 +38,31 @@ func TestUnmarshalOuterHeaderRemoval(t *testing.T) {
 		OuterHeaderRemovalDescription: OuterHeaderRemovalGtpUUdpIpv4,
 	}
 	assert.Equal(t, expectData, testData)
+}
+
+func TestUnmarshalOuterHeaderRemovalWithExtHeaderDeletion(t *testing.T) {
+	buf := []byte{OuterHeaderRemovalGtpUUdpIpv6, GtpuExtHeaderDeletionPduSessionContainer}
+	var testData OuterHeaderRemoval
+	err := testData.UnmarshalBinary(buf)
+
+	assert.Nil(t, err)
+	extHeaderDeletion := GtpuExtHeaderDeletionPduSessionContainer
+	expectData := OuterHeaderRemoval{
+		OuterHeaderRemovalDescription: OuterHeaderRemovalGtpUUdpIpv6,
+		GtpuExtensionHeaderDeletion:   &extHeaderDeletion,
+	}
+	assert.Equal(t, expectData, testData)
+}
+
+func TestOuterHeaderRemovalDescriptionValues(t *testing.T) {
+	// Verify constants match specification Table 8.2.64-1
+	assert.Equal(t, uint8(0), OuterHeaderRemovalGtpUUdpIpv4)
+	assert.Equal(t, uint8(1), OuterHeaderRemovalGtpUUdpIpv6)
+	assert.Equal(t, uint8(2), OuterHeaderRemovalUdpIpv4)
+	assert.Equal(t, uint8(3), OuterHeaderRemovalUdpIpv6)
+	assert.Equal(t, uint8(4), OuterHeaderRemovalIpv4)
+	assert.Equal(t, uint8(5), OuterHeaderRemovalIpv6)
+	assert.Equal(t, uint8(6), OuterHeaderRemovalGtpUUdpIp)
+	assert.Equal(t, uint8(7), OuterHeaderRemovalVlanStagPop)
+	assert.Equal(t, uint8(8), OuterHeaderRemovalVlanStagCtag)
 }
